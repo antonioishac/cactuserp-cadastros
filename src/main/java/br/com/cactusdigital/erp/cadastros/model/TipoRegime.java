dc99e3049c43ea0f04b30fa120cf3b763f71b72b
antonioishac <antonioishac@gmail.com>
2018-05-14 23:49:52 -0300

    Ajustes no enum TipoRegime e Ajustes na entidade Telefone

--- src/main/java/br/com/cactusdigital/erp/cadastros/model/TipoRegime.java
+++ src/main/java/br/com/cactusdigital/erp/cadastros/model/TipoRegime.java
@@ -6,12 +6,46 @@ package br.com.cactusdigital.erp.cadastros.model;
  */
 public enum TipoRegime {
 
-	LUCRO_REAL("Lucro Real"), LUCRO_PRESUMIDO("Lucro Presumido"), SIMPLES_NACIONAL("Simples Nacional");
+	LUCRO_REAL(1, "Lucro Real"), 
+	LUCRO_PRESUMIDO(2, "Lucro Presumido"), 
+	SIMPLES_NACIONAL(3, "Simples Nacional");
 
-	private String descricao;
+	private Integer codigo;
+	private String descricao;	
 
-	TipoRegime(String descricao) {
+	TipoRegime(Integer codigo, String descricao) {
+		this.codigo = codigo;
 		this.descricao = descricao;
+	}
+	
+	/**
+	 * Busca o tipo do enum pela descricao
+	 * 
+	 * @param param
+	 * @return TipoRegime
+	 */
+	public static TipoRegime parse(String param) {
+		for (TipoRegime tipoRegime : TipoRegime.values()) {
+			if (tipoRegime.name().equalsIgnoreCase(param)) {
+				return tipoRegime;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Busca o tipo do enum pelo codigo
+	 * 
+	 * @param param
+	 * @return TipoRegime
+	 */
+	public static TipoRegime parseCode(Integer param) {
+		for (TipoRegime tipoRegime : TipoRegime.values()) {
+			if (tipoRegime.ordinal() == param) {
+				return tipoRegime;
+			}
+		}
+		return null;
 	}
 
 	/**
@@ -19,15 +53,13 @@ public enum TipoRegime {
 	 */
 	public String getDescricao() {
 		return descricao;
-	}
+	}	
 
-	public static TipoRegime parse(String param) {
-		for (TipoRegime tipoRegime : TipoRegime.values()) {
-			if (tipoRegime.descricao.equalsIgnoreCase(param)) {
-				return tipoRegime;
-			}
-		}
-		return null;
+	/**
+	 * @return the codigo
+	 */
+	public Integer getCodigo() {
+		return codigo;
 	}
 
 }
